1bcace8b70e04b568b5f155c851a0046d6ac577a
RUiN-jiarun <1052707477@qq.com>
2023-01-18 21:19:00 +0800

    1.18

---
+++ 23.合并k个升序链表.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=23 lang=cpp
+ *
+ * [23] 合并K个升序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    struct comp {
+        bool operator()(ListNode* a, ListNode* b) {
+            return a->val > b->val;
+        }
+    };
+
+    priority_queue<ListNode*, vector<ListNode*>, comp> q;
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        for (auto node: lists) {
+            if (node) q.push(node);
+        }
+        ListNode* head = new ListNode();
+        ListNode* tail = head;
+        while (!q.empty()) {
+            ListNode* node = q.top();
+            q.pop();
+            tail->next = node; 
+            tail = tail->next;
+            if (node->next) q.push(node->next);
+        }
+        return head->next;
+    }
+};
+// @lc code=end
+
